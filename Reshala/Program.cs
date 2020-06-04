9502b4331b9cfea664d9ada9e9371044fc47cdd3
Anton <61422637+dolphinANTONOV@users.noreply.github.com>
2020-06-05 00:05:24 +0300

    Разработал авторизацию
    Создал отдельную форму с логином и паролем, которая подключается к базе данных

--- Reshala/Program.cs
+++ Reshala/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Reshala
+namespace authorization
 {
     static class Program
     {
@@ -16,7 +16,7 @@ namespace Reshala
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new authorization());
         }
     }
 }
